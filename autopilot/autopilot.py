dad93d72d751e33703cbb70e44216e01dcd8980e
yasser bentouila <74067276+Bentouila-yasser@users.noreply.github.com>
2023-05-31 15:38:26 +0100

    Update autopilot.py

--- autopilot/autopilot.py
+++ autopilot/autopilot.py
@@ -1,3 +1,4 @@
+import math
 import threading
 import time
 from ultra_sonic_sensor.ultra_sonic_sensor import measure_distance, setup_ultra_sonic_sensor
@@ -41,9 +42,33 @@ def calculate_bearing(lat1, lon1, lat2, lon2):
     bearing = (bearing + 360) % 360
 
     return bearing
-    
+
+#navigate function:
+def navigate(angle_difference,distance_to_obstacle):
+    if distance_to_obstacle > 2:
+        if abs(angle_difference) > 15:
+            print('changing the heading')
+            if angle_difference > 0:
+                set_speed(0,100)
+            else :
+                set_speed(100,0)
+        else : 
+            set_speed(100,100)
+    elif distance_to_obstacle > 1 : 
+        if abs(angle_difference) > 15:
+            print('changing the heading')
+            if angle_difference > 0:
+                set_speed(0,25)
+            else :
+                set_speed(25,0)
+        else : 
+            set_speed(25,25)
+    else :
+        #todo: go back and change the heading
+        print("going back and changing the heading")
+        
 #golab variable to define if  stops the thred
-def autopilot_function():
+def autopilot_function(waypoints):
     global is_autopilot_running
     is_autopilot_running = True
     print("starting the autopilot",is_autopilot_running)
@@ -53,15 +78,36 @@ def autopilot_function():
         print("autopilot started")
     # Start the autopilot loop
     while is_autopilot_running:
+        #get the current gps location
+        current_position = get_gps_data()
+        current_lat = current_position["x"]
+        current_lng = current_position["y"]
+        waypoint_lat = waypoints[0].x
+        waypoint_lng = waypoints[0].y
+        #calculate the distance between the current gps and the next waypoint
+        desired_distance = calculate_distance(current_lat, current_lng, waypoint_lat, waypoint_lng)
+        #calculate the desired bearing
+        desired_bearing = calculate_bearing(current_lat, current_lng, waypoint_lat, waypoint_lng)
+        #get the current heading
+        current_heading = get_compass_heading()
+        angle_difference = current_heading - desired_bearing
         # Check for obstacles using the ultrasonic sensor
         distance_to_obstacle = measure_distance()
-        print('distance_to_obstacle',distance_to_obstacle)
-        if distance_to_obstacle > 200 :
-            set_speed(100,100)
-        elif distance_to_obstacle > 100 :
-            set_speed(50,50)
-        else :
-            set_speed(0,0)
+        #todo:
+        #1 get the optimal path
+        #2 using the (desired_distance,desired_bearing,current_heading) command the motors
+        #3 when desired_distance is less then 1 meter, remove the waypoints[0]
+        #!note : the setspeed function sets the the percentage of each motor left and right from 0 to 100
+        #check if we reached the point
+        if desired_distance < 1:
+            print("the we reached the waypoint ",waypoints[0])
+            waypoints.pop(0)
+            if len(waypoints) == 0:
+                print("all waypoint are done !!")
+            else:
+                print("the rest of waypoints ",waypoints)
+        else:
+            navigate(angle_difference,distance_to_obstacle)
         time.sleep(1)
         
     # Stop the motors and cleanup the GPIO pins
